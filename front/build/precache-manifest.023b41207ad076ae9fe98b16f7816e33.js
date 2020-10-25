3889ad9d6c61a0efae2cf1478fb40849dcc6c42b
NetanelHp <pour55555@gmail.com>
2020-10-25 20:18:54 +0200

    deploy1

---
+++ front/build/precache-manifest.023b41207ad076ae9fe98b16f7816e33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e517bc2864d7a863e0ed1bb102cbd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c9e65c592b8d731ef4c",
+    "url": "/static/css/main.6b9138e5.chunk.css"
+  },
+  {
+    "revision": "00b4f5d8d3435974f056",
+    "url": "/static/js/2.e21f92ea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e21f92ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9e65c592b8d731ef4c",
+    "url": "/static/js/main.f52f9714.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
